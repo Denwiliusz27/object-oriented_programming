71324c0ef390cb77cec926253c609e6c37fb019f
Denwiliusz27 <danwielgosz@gmail.com>
2023-03-24 22:01:43 +0100

    zad03-kotlin: add Product class and endpoints

---
+++ zad03-kotlin/src/main/kotlin/pl/edu/uj/projobj/Product.kt
@@ -0,0 +1,11 @@
+package pl.edu.uj.projobj
+
+class Product(
+        val name: String,
+        val author: String,
+        var value: Int,
+        var amount: Int,
+        val description: String
+) {
+
+}
--- zad03-kotlin/src/main/kotlin/pl/edu/uj/projobj/ProductController.kt
+++ zad03-kotlin/src/main/kotlin/pl/edu/uj/projobj/ProductController.kt
@@ -2,18 +2,28 @@ package pl.edu.uj.projobj
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PathVariable
 
 @RestController
 class ProductController {
 
-    val test = listOf<String>("kot", "pies", "mysz")
+    val test = listOf<String>("Lalka", "Dziady", "Balladyna", "Sherlock Holmes")
+
+    val productList = listOf<Product>(
+            Product("Lalka", "Boleslaw Prus", 20, 10, "Book about Izabela"),
+            Product("Sherlock Holmes", "Artur Doyle", 15, 15, "Criminal book"),
+            Product("Hobbit", "Tolkien", 24, 13, "Book about brave hobbits and Gandalf"),
+            Product("The little prince", "Saint-Exupery", 30, 20, "")
+    )
+
+
     @GetMapping("/")
-    fun test(): String {
-        return "Hello world"
+    fun getAllProducts(): List<Product> {
+        return productList
     }
 
     @GetMapping("/{id}")
-    fun getAnimal(int Id): String {
-        return test[id]
+    fun getProduct(@PathVariable id: Int): Product? {
+        return productList.getOrNull(id)
     }
 }
